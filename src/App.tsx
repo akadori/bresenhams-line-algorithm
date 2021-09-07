19658f106e3859a897399ea0b12bf595058cf099
tamaritamari <tamari.katamari@gmail.com>
2021-09-07 12:45:17 +0900

    ここから先どうしたいのか

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,55 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useRef, MouseEvent, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [windowSplitSize, setWindowSplitSize] = useState({ width: window.innerWidth / 2, height: window.innerHeight })
+  type Point = {
+    x: number,
+    y: number
+  }
+  const [points, setPoints] = useState<Array<Point>>([])
+  // window.addEventListener('resize', () => setWindowSplitSize({width: window.innerWidth / 2, height: window.innerHeight })) めっちゃ重い。debounceとかするんだっけ? useLayoutEffect?
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const canvasRef2 = useRef<HTMLCanvasElement>(null)
+
+  const handleOnclick = (e: MouseEvent<HTMLCanvasElement>) => {
+    const { clientX, clientY } = e;
+    if (points.length === 0) {
+      setPoints([{ x: clientX, y: clientY }]);
+    } else if (points.length === 1) {
+      setPoints(prev => [prev[0], { x: clientX, y: clientY }])
+    } else {
+      setPoints(prev => [...prev.slice(1), { x: clientX, y: clientY }]);
+    }
+  }
+
+  useEffect(() => {
+    draw(canvasRef.current!)
+    draw(canvasRef2.current!)
+  }, [points])
+
+  const draw = (canvas: HTMLCanvasElement) => {
+    const context = canvas.getContext("2d")!;
+    context.clearRect(0, 0, windowSplitSize.width, windowSplitSize.height);
+    points.forEach((point) => {
+      if (point) {
+        context.fillStyle = "#000000";
+        context.beginPath();
+        context.arc(point.x, point.y, 5, 0, 2 * Math.PI);
+        context.fill();
+      }
+    });
+    if (points.length === 2) {
+      context.beginPath();
+      context.moveTo(points[0].x, points[0].y);
+      context.lineTo(points[1].x, points[1].y);
+      context.stroke();
+    }
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div style={{ display: 'flex' }}>
+      <canvas ref={canvasRef} width={windowSplitSize.width} height={windowSplitSize.height} style={{ border: "1px black solid" }} onClick={handleOnclick}></canvas>
+      <canvas ref={canvasRef2} width={windowSplitSize.width} height={windowSplitSize.height} style={{ border: "1px black solid" }} onClick={handleOnclick}></canvas>
     </div>
   )
 }
